aadf90298ca9b5c001321651bef54975c7ca2c0f
Philip Sharp <philip@kerzap.com>
2013-03-29 22:16:58 -0700

    Add height/width URL parameters only if not templated

--- js/util.js
+++ js/util.js
@@ -23,14 +23,18 @@ Slapdash.Util = {
         var url = graph.baseUrl + graph.url,
             width = Slapdash.Util.getSize(layoutElement[0]),
             height = Slapdash.Util.getSize(layoutElement[1]);
-            params = _.extend({},graph.parameters,{width:width,height:height});
-        url += (url.indexOf("?") >= 0) ? "&" : "?";
-        url += $.param(params);
+            params = graph.parameters;
         
         if (graph.templateUrl){
             url = url.replace(Slapdash.Util._urlTemplates.height, height);
             url = url.replace(Slapdash.Util._urlTemplates.width, width);
         }
+        else {
+            _.extend(params, {width:width, height:height});
+        }
+        
+        url += (url.indexOf("?") >= 0) ? "&" : "?";
+        url += $.param(params);
         
         return url;
     }
